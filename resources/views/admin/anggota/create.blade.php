1e6068efc6fb0a868bdcf599ef9d6dbc3e7ca006
Adeqq1 <aderifqyreg.ti2@gmail.com>
2026-05-12 07:13:33 +0700

    update class button on text "batal"

--- resources/views/admin/anggota/create.blade.php
+++ resources/views/admin/anggota/create.blade.php
@@ -90,7 +90,7 @@
                     <button type="submit" class="btn btn-primary">
                         <i class="bi bi-save"></i> Simpan
                     </button>
-                    <a href="{{ route('admin.anggota.index') }}" class="btn btn-outline-secondary">
+                    <a href="{{ route('admin.anggota.index') }}" class="btn btn-danger">
                         Batal
                     </a>
                 </div>
