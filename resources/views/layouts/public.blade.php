16066463e41a6ba64391d7ced5db1a39655e7987
Adeqq1 <aderifqyreg.ti2@gmail.com>
2026-09-19 00:33:04 +0700

    update title of tab

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>IMM | Ikatan Mahasiswa Muhammadiyah</title>
+    <title>IMM | Ikatan Mahasiswa Muhammadiyah Bungo</title>
     <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
 
     {{-- SEO Meta --}}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <title>SIM-IMM | Keanggotaan & Kearsipan</title>
+        <title>IMM | Ikatan Mahasiswa Muhammadiyah Bungo</title>
         
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
